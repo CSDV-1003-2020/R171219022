f9ce94f01374252b8f630b85b7b9908830ae0922
PHAGUN-JAIN <omgill084@gmail.com>
2020-03-27 23:59:36 +0530

    wrote comments once again

--- loop.c
+++ loop.c
@@ -4,8 +4,8 @@ main()
    int i,j,l=5;
      for(i=1;i<=l;i++)
    {
-      printf("\n");
-      for(j=1;j<=l;j++)//nested for statement
+      printf("\n");//for transfering cursor to next line
+      for(j=1;j<=l;j++)//inested for statement
       printf("%d",i);//for printing an integer
    }
  
