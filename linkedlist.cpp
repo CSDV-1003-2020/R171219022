65ab8ac7a5bee39764bc47ddcb97a9e3a9f90010
PHAGUN-JAIN <omgill084@gmail.com>
2020-03-29 15:30:09 +0530

    declared a node class

---
+++ linkedlist.cpp
@@ -0,0 +1,11 @@
+
+
+#include<iostream>
+using namespace std;
+class Node
+{
+  public:
+	  int data;
+	  Node *next;//declaring a pointer of node type (pointer is nothing but a variable which stores address of other variable)
+};
+
