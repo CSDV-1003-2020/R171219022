a1235a9d74ed8b736af7c57ada36dd475e2669cd
PHAGUN JAIN <53911145+PHAGUN-JAIN@users.noreply.github.com>
2020-04-20 17:51:32 +0530

    for merge

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -3,8 +3,5 @@
 using namespace std;
 class Node
 {
-  public:
-	  int data;
-	  Node *next;//declaring a pointer of node type (pointer is nothing but a variable which stores address of other variable)
-};
+  public
 
