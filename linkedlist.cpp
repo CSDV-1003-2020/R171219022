5ff2efd808c3180aaa47b8097c22fab5d46d2dc1
Pranit5895 <guptapranit9@gmail.com>
2020-04-20 17:42:13 -0700

    Code updated and push function added

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -5,6 +5,27 @@ class Node
 {
   public:
 	  int data;
-	  Node *next;//declaring a pointer of node type (pointer is nothing but a variable which stores address of other variable)
-};
+	  Node *next;//declaring a pointer of node type (pointer is nothing but a variable whic
+	  struct node
+{
+  int item;
+  node*next;
+}*front,*rear,*newptr,*save,*ptr;
+node* create_node(int n)
+{
+ptr=new node;
+ptr->item=n;
+ptr->next=NULL;
+return ptr;
+}
+void insert(node*np)
+{
+if(front==NULL)
+front=rear=np;
+else
+{
+rear->next=save;
+rear=np;
+}
+}
 
